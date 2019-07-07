87d05e771db9b168b6215f08becaf29827f328b0
rinakawamura <rina.kawamura@yale.edu>
2019-07-07 17:24:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let element = document.getElementById('main');
+element.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'Rina is the champion';
