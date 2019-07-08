8238b1c917ad257303801c90e0215ba3a3fb3a4d
XRuiz9 <xdruiz92@gmail.com>
2019-07-08 14:12:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let mainTag = document.getElementById('main');
+mainTag.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Xavier is the champion";
