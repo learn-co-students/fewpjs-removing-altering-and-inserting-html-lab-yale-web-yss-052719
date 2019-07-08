aba0cbd09720791b5175ffc612d36d2f781d0cae
maxsunnyday <max.sun@yale.edu>
2019-07-08 15:17:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = `Max is the champion`;
+document.body.appendChild(newHeader);
