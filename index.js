767516f067206b9b6192d1ab026838ed94f0f45a
Elijah <efloren@siue.edu>
2019-07-08 14:31:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+var maine = document.querySelector("main#main");
+maine.remove();
+
+var newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = ('Elijah is the champion')
+document.body.append(newHeader)
+
