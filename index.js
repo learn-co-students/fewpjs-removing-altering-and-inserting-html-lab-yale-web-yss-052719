700accba3ddd22c922b5f8af6b8395ba1b38b9ff
Alexandro Alanya <alexandro.alanya@gmail.com>
2019-07-16 15:10:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.addEventListener('DOMContentLoaded', function(){
+  
+})
+document.body.appenChild(element)
+
+var ul = document.createElement('ul')
+
+
+element.appendChild(ul)
+
+//let element = document.querySelector("p#greeting");
+//element.innerHTML = 'Hello, DOM!'
